62b6ee929c92867fc0da9b22478f21f5eba61c30
FangfangZhao <zhaofang123@gmail.com>
2018-11-17 00:33:53 +0800

    Update CascadeModel.java

--- src/main/java/cn/devezhao/persist4j/metadata/CascadeModel.java
+++ src/main/java/cn/devezhao/persist4j/metadata/CascadeModel.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 
+ * 级联删除模式
  * 
  * @author <a href="mailto:zhaofang123@gmail.com">FANGFANG ZHAO</a>
  * @since 0.1, Feb 15, 2009
@@ -12,18 +12,33 @@ import java.util.Map;
  */
 public enum CascadeModel {
 
-	Delete, RemoveLinks, Ignore;
+	/**
+	 * 级联删除
+	 */
+	Delete,
+	/**
+	 * 移除链接
+	 */
+	RemoveLinks,
+	/**
+	 * 忽略，啥都不干
+	 */
+	Ignore;
 	
-	
-	static Map<String, CascadeModel> cmMap = new HashMap<String, CascadeModel>();
+	private static final Map<String, CascadeModel> NAMED = new HashMap<String, CascadeModel>();
 	static {
-		cmMap.put("delete", Delete);
-		cmMap.put("remove-links", RemoveLinks);
-		cmMap.put("ignore", Ignore);
+		NAMED.put("delete", Delete);
+		NAMED.put("remove-links", RemoveLinks);
+		NAMED.put("removelinks", RemoveLinks);
+		NAMED.put("ignore", Ignore);
 	}
 	
+	/**
+	 * @param model
+	 * @return
+	 */
 	public static CascadeModel parse(String model) {
-		CascadeModel cm = cmMap.get(model);
+		CascadeModel cm = NAMED.get(model.toLowerCase());
 		if (cm != null) {
 			return cm;
 		}

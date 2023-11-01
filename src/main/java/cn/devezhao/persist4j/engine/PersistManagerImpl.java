0b293fa74b124b1a017b6dde966f472f1aa6ac18
RB <getrebuild@sina.com>
2023-11-01 11:12:56 +0800

    fix: QueryedRecord cannot be used for save/update

--- src/main/java/cn/devezhao/persist4j/engine/PersistManagerImpl.java
+++ src/main/java/cn/devezhao/persist4j/engine/PersistManagerImpl.java
@@ -47,7 +47,7 @@ public class PersistManagerImpl extends JdbcSupport implements PersistManager {
 	@Override
 	public Record save(final Record record) throws DataAccessException {
 		Validate.isTrue(record.getPrimary() == null);
-		Validate.isTrue(record instanceof QueryedRecord, "QueryedRecord cannot be used for save");
+		Validate.isTrue(!(record instanceof QueryedRecord), "QueryedRecord cannot be used for save");
 
 		Entity e = record.getEntity();
 		return saveInternal(record, ID.newId(e.getEntityCode()));
@@ -134,7 +134,7 @@ public class PersistManagerImpl extends JdbcSupport implements PersistManager {
 	@Override
 	public Record update(final Record record) throws DataAccessException {
 		Validate.notNull(record.getPrimary());
-		Validate.isTrue(record instanceof QueryedRecord, "QueryedRecord cannot be used for update");
+		Validate.isTrue(!(record instanceof QueryedRecord), "QueryedRecord cannot be used for update");
 
 		Entity e = record.getEntity();
 		
